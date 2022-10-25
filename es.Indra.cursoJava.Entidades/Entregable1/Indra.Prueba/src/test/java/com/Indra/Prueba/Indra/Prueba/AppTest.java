26e4692707d75c90e3dada4b16ed2d2a34230aa0
HelioX4 <74423956+JuanmaX4@users.noreply.github.com>
2022-10-25 08:07:57 +0200

    Update Maven

---
+++ es.Indra.cursoJava.Entidades/Entregable1/Indra.Prueba/src/test/java/com/Indra/Prueba/Indra/Prueba/AppTest.java
@@ -0,0 +1,20 @@
+package com.Indra.Prueba.Indra.Prueba;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
