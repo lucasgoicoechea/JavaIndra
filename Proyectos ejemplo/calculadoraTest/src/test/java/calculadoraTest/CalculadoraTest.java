a80f9a8dc234d5e4cc015ba734802da010e4dabb
lucasgoicoechea <lucasgoicoechea@gmail.com>
2022-10-28 10:47:10 +0200

    ultimos cambios

--- Proyectos ejemplo/calculadoraTest/src/test/java/calculadoraTest/CalculadoraTest.java
+++ Proyectos ejemplo/calculadoraTest/src/test/java/calculadoraTest/CalculadoraTest.java
@@ -1,6 +1,7 @@
 package calculadoraTest;
 
 import static org.junit.Assert.*;
+import static org.junit.Assume.assumeThat;
 
 import org.junit.After;
 import org.junit.Before;
@@ -35,5 +36,15 @@ public class CalculadoraTest {
 		assertEquals("Ha fallado la suma basica", ca.suma(-1, -1), -2);
 		assertEquals("Ha fallado la suma basica", -11001, ca.suma(-1, -11000));
 	}
+	
+	@Test
+	public final void testSumarizado() { 
+		int[] arrnros = {10,20,30};
+		int[] xpected = {20,30,40};
+		assertArrayEquals(xpected, ca.sumarizado(arrnros, 10));	
+		xpected[2] = 0;
+		assertArrayEquals(xpected, ca.sumarizado(arrnros, 10));	
+		//assumeThat(xpected, ca.sumarizado(arrnros, 10));		
+ 	}
 
 }
