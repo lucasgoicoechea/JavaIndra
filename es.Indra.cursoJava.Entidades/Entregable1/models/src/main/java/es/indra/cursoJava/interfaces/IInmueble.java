81cd7696b8d1036b328084ee5da11d4937d0b65a
HelioX4 <74423956+JuanmaX4@users.noreply.github.com>
2022-11-08 10:16:52 +0100

    Update Entidades

--- es.Indra.cursoJava.Entidades/Entregable1/models/src/main/java/es/indra/cursoJava/interfaces/IInmueble.java
+++
@@ -1,9 +0,0 @@
-package es.indra.cursoJava.interfaces;
-
-import es.indra.cursoJava.models.Inventario;
-
-public interface IInmueble {
-	
-	public String getDireccion();
-	public Inventario getInventario();
-}
