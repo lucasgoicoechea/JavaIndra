5d73855cda49f95c260941a68b32924ca60b2807
eric <ejguerrar@indra.es>
2022-11-11 10:55:55 +0100

    PRIMER COMMIT

---
+++ EXAMEN/AspectosEjemplos/src/main/java/es/indra/AspectosEjemplosApplication.java
@@ -0,0 +1,13 @@
+package es.indra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspectosEjemplosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AspectosEjemplosApplication.class, args);
+	}
+
+}
